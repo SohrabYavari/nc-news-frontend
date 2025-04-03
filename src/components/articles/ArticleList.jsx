cc0c3b9be7406a32648201ff8f9008871e1e8412
SohrabYavari <sohrabyavari00@gmail.com>
2025-04-03 14:28:34 +0100

    filter through topics

--- src/components/articles/ArticleList.jsx
+++ src/components/articles/ArticleList.jsx
@@ -3,6 +3,10 @@ import { Link } from "react-router-dom";
 import Card from "../global/Card";
 
 export default function ArticleList({ articles }) {
+  if (!articles || articles.length === 0) {
+    return <p className="text-center p-4">No articles available</p>;
+  }
+
   return articles.map((article) => (
     <Link key={article.article_id} to={`/home/articles/${article.article_id}`}>
       <Card
